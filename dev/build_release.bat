5d5211ec67e278ad273c779d8b7ce798568b2a8c
jpr65 <peine@Peine-PC.dmrz.de>
2015-10-30 08:45:06 +0100

    release Alive and Spartanic
    release Alive and Spartanic

--- dev/build_release.bat
+++ dev/build_release.bat
@@ -1,3 +1,6 @@
 @rem
 
-perl -I../../AndroidPerl/spartanic/lib -I/storage/emulated/legacy/CCTools/Perl/CPAN/lib build_release.pl -module Perl5-Spartanic -version 0.110 -dest ../rel
+@rem perl -I../../AndroidPerl/spartanic/lib -I/storage/emulated/legacy/CCTools/Perl/CPAN/lib build_release.pl -module Perl5-Spartanic -version 0.110 -dest ../rel
+
+perl -I../../AndroidPerl/spartanic/lib -I/storage/emulated/legacy/CCTools/Perl/CPAN/lib build_release.pl -module Alive -version 0.110 -dest ../rel
+
